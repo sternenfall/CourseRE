5b95af50fc68fd617c0e5a1aa8a4ea63234939de
sternenfall <sternenfall.1995@gmail.com>
2024-06-10 17:20:54 +0900

    マイグレーションファイルの作成

---
+++ database/migrations/2024_06_10_080511_create_faculties_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('faculties', function (Blueprint $table) {
+            $table->id('faculty_id');
+            $table->string('name', 200);
+            $table->decimal('credit', $precision = 4, $scale = 1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('faculties');
+    }
+};
---
+++ database/migrations/2024_06_10_081033_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->string('name', 200);
+            $table->string('email', 200);
+            $table->string('password', 200);
+            $table->timestamps('created_at');
+            $table->timestamps('updated_at');
+            $table->timestamps('email_verified_at');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
